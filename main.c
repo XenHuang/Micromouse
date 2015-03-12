fb1d6712bf4b227b9fe4df5e590ca6e8c30faf0e
kunliangxiao <kunliang@hawaii.edu>
2015-03-11 15:09:40 -1000

    Motor driven by Timer0 interruption testing

--- main.c
+++ main.c
@@ -42,7 +42,8 @@ void delay(unsigned int ms){
         __delay_ms(1);
 }
 
-     int sensor;
+    int sensor;
+	int motor;
      
 void main(void)
 {
@@ -56,27 +57,62 @@ void main(void)
 
     /* Initialize I/O and Peripherals for application */
     InitApp();
-    
-     TRISB=2;
-     TRISD=0;
-     ADCON1 =1;
-     ADCON2=0b00001010;
-     ADCON0=0b00101011;
-     
+   
+	T0CON = 0b11000000;
+		//7.TMR0ON: 1 = Enables Timer()
+		//6.T08BIT: 1 = 8-bit timer, 0 = 16-bit
+		//5.T0CS: 0 = Internal instruction cycle clock(CLKO, not T0CKI)
+		//4.T0SE: 0 = increment on low-to-high transition onT0CKI pin
+		//3.PSA: 0 = Using Timer() prescaler
+		//2-0. T0PS2:T0PS0: 000 = 1:2 ------ 111 = 1:256 prescale value
+
+	INTCON2 = 0b11110100;
+		//7.RBPU: 1 = All PORTB pull-ups are disabled
+		//6.INTEDG0: 1 = interrupt on rising edge for interrupt 0
+		//5.INTEDG1: same as above but for 1
+		//4.INTEDG2: same as above but for 2
+		//3.Unimplemented
+		//2.TMR0IP:	1 = TMR0 Overflow interrupt = High (on Logic Diagram)
+		//1.Unimplemented
+		//0.RBIP: 1 = RB port change interrupt = High (on Logic Diagram)
+
+	INTCON = 0b00100000;
+		//7.GIE/GIEH: 1 = Enable interrupts
+		//6.PEIE/GIEL 1 = Enable peripheral interrupts
+		//5.TMR0IE 1 = Enable TMR0 Overflow interrupt
+		//4.INT0IE 1 = Enable INT0 interrupt
+		//3.RBIE 1 = Enable RB port change interrupt
+		//2.TMR0IF 1 = TMR0 Overflow interrupt flag 
+		//1.INT0IF 1 = INT0 interrupt flag
+		//0.RBIF 1 = RB port change interrupt flag
+
+	INTCONbits.GIE = 1;
+
+	TRISC = 0;
+ 
     while(1)
     {
-        
-        ADCON0bits.GO_DONE = 1;
-        while(ADCON0bits.GO_DONE != 0); //Loop here until A/D conversion completes 
-        sensor = (ADRESH << 2) + (ADRESL >> 6);
-        
-        
-        if(ADRESL > 100)
-            LATD=1;
-        else
-            LATD=0;
-       
+		        
     }
 
 }
 
+void intterrupt motro()
+{
+	if(INTCONbits.TMR0IF == 1)
+	{
+		if(motor < 4)
+		motor++;
+		else
+		motor = 0;
+
+		switch(motor)
+		{
+			case 0: LATC = 0b10001000;
+			case 1: LATC = 0b01000100;
+			case 2: LATC = 0b00100010;
+			case 3: LATC = 0b00010001;
+		}	
+		INTCONbits.TMR0IF = 0;
+	} 
+}
