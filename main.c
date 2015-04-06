0dcfde47c95f6a03718fd77f0c5f664065e0d4a7
kunliang <kunliang@hawaii.edu>
2015-04-05 17:45:22 -1000

    actual code developing, pre-test, motor drive, sensors update

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 /* Files to Include                                                           */
 /******************************************************************************/
 
+
 #if defined(__XC)
     #include <xc.h>        /* XC8 General Include File */
 #elif defined(HI_TECH_C)
@@ -25,7 +26,6 @@
 #include <pic18f4620.h>
 #include <time.h>
 #include <xc.h>
-#define _XTAL_FREQ 32000000
 /******************************************************************************/
 /* User Global Variable Declaration                                           */
 /******************************************************************************/
@@ -35,63 +35,22 @@
 /******************************************************************************/
 /* Main Program                                                               */
 /******************************************************************************/
-int delay_time = 0;
-void delay(unsigned int ms){
-    int x = 0;
-    for (x = 0; x < ms; x++)
-        __delay_ms(1);
-}
 
-    int sensor;
+
      
 void main(void)
-{
-    //enable 8MHz internal clock
-    OSCCON = 0b01111100;
-    //enable internal PLL clock = 32MHz
-    OSCTUNE = 0b01001111;
-   
+{   
     /* Configure the oscillator for the device */
     ConfigureOscillator();
 
     /* Initialize I/O and Peripherals for application */
     InitApp();
-   
-	T0CON = 0b11000100;
-		//7.TMR0ON: 1 = Enables Timer()
-		//6.T08BIT: 1 = 8-bit timer, 0 = 16-bit
-		//5.T0CS: 0 = Internal instruction cycle clock(CLKO, not T0CKI)
-		//4.T0SE: 0 = increment on low-to-high transition onT0CKI pin
-		//3.PSA: 0 = Using Timer() prescaler
-		//2-0. T0PS2:T0PS0: 000 = 1:2 ------ 111 = 1:256 prescale value
-
-	INTCON2 = 0b11110100;
-		//7.RBPU: 1 = All PORTB pull-ups are disabled
-		//6.INTEDG0: 1 = interrupt on rising edge for interrupt 0
-		//5.INTEDG1: same as above but for 1
-		//4.INTEDG2: same as above but for 2
-		//3.Unimplemented
-		//2.TMR0IP:	1 = TMR0 Overflow interrupt = High (on Logic Diagram)
-		//1.Unimplemented
-		//0.RBIP: 1 = RB port change interrupt = High (on Logic Diagram)
-
-	INTCON = 0b00100000;
-		//7.GIE/GIEH: 1 = Enable interrupts
-		//6.PEIE/GIEL 1 = Enable peripheral interrupts
-		//5.TMR0IE 1 = Enable TMR0 Overflow interrupt
-		//4.INT0IE 1 = Enable INT0 interrupt
-		//3.RBIE 1 = Enable RB port change interrupt
-		//2.TMR0IF 1 = TMR0 Overflow interrupt flag 
-		//1.INT0IF 1 = INT0 interrupt flag
-		//0.RBIF 1 = RB port change interrupt flag
-    delay(3000);
-	INTCONbits.GIE = 1;
 
 	TRISC = 0;
  
     while(1)
     {
-		        
+	sensorUpdate();
     }
 
 }
--- user.h
+++ user.h
@@ -3,11 +3,31 @@
 /******************************************************************************/
 
 /* TODO Application specific user parameters used in user.c may go here */
+#define _XTAL_FREQ 32000000
+
+#define SENSORCOMPUTATION 10    //the number of times averaging sensor value, min = 3
+#define NUMSENSORS 4
+#define LEFTSENSORL 0111         //ADCON0 value
+#define RIGHTSENSORL 1100        //ADCON0 value
+#define LEFTFRONTSENSORL 0110    //ADCON0 value
+#define RIGHTFRONTSENSORL 1010   //ADCON0 value
+const unsigned char sensorLocation[NUMSENSORS] = {LEFTSENSORL,RIGHTSENSORL,LEFTFRONTSENSORL,RIGHTFRONTSENSORL};
+
+typedef enum {LEFTWALL,RIGHTWALL} Algorithm;
+Algorithm algorithm;
+
+int delay_time = 0;
+int sensorValue[NUMSENSORS];
+
 
 /******************************************************************************/
 /* User Function Prototypes                                                   */
 /******************************************************************************/
 
 /* TODO User level functions prototypes (i.e. InitApp) go here */
-
+void delay(unsigned int);
 void InitApp(void);         /* I/O and Peripheral Initialization */
+void sensorUpdate();
+void sensorComputation(int[][SENSORCOMPUTATION]);
+int MIN(int,int);
+int MAX(int,int);
